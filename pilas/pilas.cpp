8d608034cbef203fc9410de52442cbe50e823ad2
ORTEGA NAVEDO FELIPE DE JESUS <zs24022200@estudiantes.uv.mx>
2026-04-22 13:46:51 -0600

    Menú temrinado sobre las pilas

--- pilas/pilas.cpp
+++ pilas/pilas.cpp
@@ -12,29 +12,76 @@ class pilas
 private:
     Nodo* top;
 public:
-    pilas(/* args */);
-    ~pilas();
+    pilas(){
+        top = nullptr;
+    }
+    bool isEmpty() {
+        return top == nullptr;
+    }
+    void push(int valor) {
+        Nodo* nuevo = new Nodo();
+        nuevo->dato = valor;
+        nuevo->siguiente = top;
+        top = nuevo;
+    }
+    void pop() {
+        if (isEmpty()) {
+            cout << "La pila esta vacia. No se puede eliminar." << endl;
+            return;
+        }
+        Nodo* auxiliar = top;
+        cout << "Elemento eliminado: " << top->dato << endl;
+        top = top->siguiente;
+        delete auxiliar;
+    }
+    void topElement() {
+        if (isEmpty()) {
+            cout << "La pila esta vacia." << endl;
+            return;
+        }
+        cout << "Elemento en la cima: " << top->dato << endl;
+    }
+    void imprimir() {
+        if (isEmpty()) {
+            cout << "La pila esta vacia." << endl;
+            return;
+        }
+        Nodo* auxiliar = top;
+        while (auxiliar != nullptr) {
+            cout << auxiliar->dato << " ";
+            auxiliar = auxiliar->siguiente;
+        }
+        cout << endl;
+    }
+    void contar () {
+        int contador = 0;
+        Nodo* auxiliar = top;
+        while (auxiliar != nullptr) {
+            contador++;
+            auxiliar = auxiliar->siguiente;
+        }
+        cout << "Cantidad de elementos en la pila: " << contador << endl;
+    }
+    void vaciar() {
+        while (top != nullptr) {
+            Nodo* auxiliar = top;
+            top = top->siguiente;
+            delete auxiliar;
+        }
+    }
 };
 
-pilas::pilas(/* args */)
-{
-    top = nullptr;
-}
+void menu() {
+    cout << "1. Push (insertar)" << endl;
+    cout << "2. Pop (eliminar)" << endl;
+    cout << "3. Top (mostrar cima)" << endl;
+    cout << "4. Imprimir pila" << endl;
+    cout << "5. Contar elementos" << endl;
+    cout << "6. Vaciar pila" << endl;
+    cout << "0. Salir" << endl;
+};
 
-pilas::~pilas()
-{
-}
-
-void menu()
-{
-    cout << "1. Agregar elemento a la pila" << endl;
-    cout << "2. Eliminar elemento de la pila" << endl;
-    cout << "3. Mostrar elementos de la pila" << endl;
-    cout << "4. Salir" << endl;
-}
-
-int main()
-{
+int main() {
     pilas pila;
     int valor, opcion;
 
@@ -47,26 +94,26 @@ int main()
             case 1:
                 cout << "Ingrese un valor: ";
                 cin >> valor;
-                // Agregar valor a la pila
+                pila.push(valor);
                 break;
             case 2:
-                // Eliminar elemento de la pila
+                pila.pop();
                 break;
             case 3:
-                // Mostrar elementos de la pila
+                pila.topElement();
                 break;
             case 4:
-                cout << "Saliendo..." << endl;
+                pila.imprimir();
                 break;
+            case 5:
+                pila.contar();
+                break;
+            case 6:
+                pila.vaciar();
             default:
                 cout << "Opcion no valida. Intente de nuevo." << endl;
         }
-    } while (opcion != 4);
-
-
-
-
-
+    } while (opcion != 0);
 
     return 0;
 }
